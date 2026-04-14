93df0b616e108d9a88192eddfa7ef33f7ea1316f
Wesley Santos <wesleysantossilva.2019.2@gmail.com>
2026-04-13 21:13:29 -0300

    Adiciona MySQL e Dockerfile customizado

--- app/index.php
+++ app/index.php
@@ -1,2 +1,14 @@
 <?php
-echo "Olá, mundo!";
+
+$host = "mysql";
+$user = "root";
+$password = "root";
+$database = "testdb";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+
+echo "Conectado ao MySQL com sucesso!";
